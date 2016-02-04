715e93063aea6bf0dada0e9bb3700a37c1a2f504
oguzCoel <oguzcoel@gmail.com>
2016-02-04 01:15:43 +0100

    sqlparser extendet to read multi row parameter

--- js/sqlParser.js
+++ js/sqlParser.js
@@ -1,23 +1,11 @@
 sqlParser = Class.extend(function(){
 	this.constructor = function(){
-	}
-	
-	
-	
+	};
+
 	this.getMVQ = function(sql,model){
-		
-		console.log( model.getHeaderByName('ID') )
-		
-		// function select data
-		// Col.fSelect(arg)
-		// arg = column index
-		// returns column from model
-		
-		
-		// Col.fUpCase(arg)
-		// arg = string
-		// returns upcase of string
-		
+
+		console.log( model.getHeaderByName('Year') );
+		var col = [];
 
 		var sqlQuery = sql.split(" ");
 
@@ -28,36 +16,41 @@ sqlParser = Class.extend(function(){
 			console.log("wrong query input");
 		}
 
-		if ((sqlQuery[3] == "as" && sqlQuery[4] == "select") && (sqlQuery[5] == data[0][0][0] || sqlQuery[5] == data[0][1][0] || sqlQuery[5] == data[0][2][0])) {
-			switch (true) {
-				case sqlQuery[5] == data[0][0][0]:
-					var col = new Col.fSelect(0);
-					break;
-				case sqlQuery[5] == data[0][1][0]:
-					var col = new Col.fSelect(1);
-					break;
-				case sqlQuery[5] == data[0][2][0]:
-					var col = new Col.fSelect(2);
-					break;
-				default:
-					console.log("Something goes wrong");
+		if (sqlQuery[3] == "as" && sqlQuery[4] == "select" ){
+			var tmp = [];
+			tmp = sqlQuery.slice(5);
+			var str = new String();
+			for(var i=0; i<tmp.length; i++){ str += tmp[i];}
+			tmp = str.split(",");
+			var rowNames = tmp; }
+
+			for(var i=0; i<rowNames.length; i++){
+				console.log(i+1 + " row: " + rowNames[i]);
+				if(rowNames[i] == data[0][0][0] || rowNames[i] == data[0][1][0] || rowNames[i] == data[0][2][0]){
+					switch (true) {
+						case rowNames[i] == data[0][0][0]:
+							col.push(new Col.fSelect(0));
+							break;
+						case rowNames[i] == data[0][1][0]:
+							col.push(new Col.fSelect(1));
+							break;
+						case rowNames[i] == data[0][2][0]:
+							col.push( new Col.fSelect(2));
+							break;
+						default:
+							console.log("Something goes wrong");
+					}
+				}
+				else {
+					console.log("wrong query input")
+				}
 			}
+
+			query = new MVQuery( {id:tableName} , {select: col } );
+
+			console.log("Query: " + sql);
+			console.log("size of col array: " + col.length);
+
+			return query;
 		}
-		else {
-			console.log("wrong query input")
-		}
-
-
-
-
-		
-
-		
-		var query = new MVQuery( {id:tableName} , [col]);
-		console.log(sql);
-
-				
-		return query;
-	}
-	
-})
+	});
